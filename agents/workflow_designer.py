083f42daa27fccf29bad5c69a990ce39566ae750
Haitam BORQANE <haytamborquane@gmail.com>
2026-05-23 16:40:10 +0100

    add workflow designing agent with the appropriate skills

---
+++ agents/workflow_designer.py
@@ -0,0 +1,20 @@
+from agno.agent import Agent
+from models import get_model
+from db import get_postgres_db
+from prompts.agent_instructions import WORKFLOW_DESIGNER_INSTRUCTIONS
+from agno.skills import Skills, LocalSkills
+
+
+workflow_designer = Agent(
+    id="workflow-designer",
+    name="WorkflowDesigner",
+    model=get_model(),
+    db=get_postgres_db(),
+    instructions=WORKFLOW_DESIGNER_INSTRUCTIONS,
+    skills=Skills(loaders=[LocalSkills("skills/workflow_design_skills/")]),
+    enable_agentic_memory=True,
+    add_datetime_to_context=True,
+    add_history_to_context=True,
+    num_history_runs=5,
+    markdown=True,
+)
