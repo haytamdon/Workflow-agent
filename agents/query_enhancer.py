4a6782bb79cac2156c7f1456982dccafc5c19697
Haitam BORQANE <haytamborquane@gmail.com>
2026-05-23 16:39:42 +0100

    add query enhancement agent

---
+++ agents/query_enhancer.py
@@ -0,0 +1,18 @@
+from agno.agent import Agent
+from models import get_model
+from db import get_postgres_db
+from prompts.agent_instructions import QUERY_ENHANCER_INSTRUCTIONS
+
+
+query_enhancer = Agent(
+    id="query-enhancer",
+    name="QueryEnhancer",
+    model=get_model(),
+    db=get_postgres_db(),
+    instructions=QUERY_ENHANCER_INSTRUCTIONS,
+    enable_agentic_memory=True,
+    add_datetime_to_context=True,
+    add_history_to_context=True,
+    num_history_runs=5,
+    markdown=True,
+)
