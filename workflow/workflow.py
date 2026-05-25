215e8b67193a26cbb8d48bb8b2a994928a0ec8e4
Haitam BORQANE <haytamborquane@gmail.com>
2026-05-25 15:15:41 +0100

    define initial workflow

---
+++ workflow/workflow.py
@@ -0,0 +1,16 @@
+from agno.workflow import Parallel, Step, Workflow
+from agents.query_enhancer import query_enhancer
+from agents.workflow_designer import workflow_designer
+from agents.workflow_creator import workflow_creator
+from agents.workflow_validator import workflow_validator
+
+n8n_workflow_creation = Workflow(
+    id="n8n-workflow-creation",
+    name="N8N workflow creation",
+    steps=[
+        Step(name="Query Enhancer", agent=query_enhancer),
+        Step(name="Workflow Designer", agent=workflow_designer),
+        Step(name="Workflow Creator", agent=workflow_creator),
+        Step(name="Workflow Validator", agent=workflow_validator),
+    ],
+)
